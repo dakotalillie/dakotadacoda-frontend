5a0aea803a7b86d254eabdc3a77a55408a4147ee
Dakota Lillie <dakota.lillie@icloud.com>
2018-03-28 18:05:13 -0400

    add header

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import FlashMessage from './flash_message/FlashMessage';
+import Header from './header/Header';
 
 const Welcome = () => {
   return (
     <div>
       <FlashMessage />
+      <Header />
     </div>
-  )
-}
+  );
+};
 
 export default Welcome;
---
+++ src/components/welcome/header/Header.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  Collapse,
+  Container,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+class Header extends React.Component {
+  state = {
+    isOpen: false
+  };
+
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <Container>
+          <Navbar color="faded" light expand="md">
+            <NavbarBrand href="#">Logo</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">About</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">Portfolio</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">Resume</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">Blog</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">Contact</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Header;
