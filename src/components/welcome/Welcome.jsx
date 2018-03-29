82fbd9e82f39580975d51813652c7a62abd5e57b
Dakota Lillie <dakota.lillie@icloud.com>
2018-03-29 18:02:36 -0400

    add basic portfolio preview

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -3,6 +3,7 @@ import FlashMessage from './flash_message/FlashMessage';
 import Header from './header/Header';
 import Jumbo from './jumbo/Jumbo';
 import AboutPreview from './about_preview/AboutPreview';
+import PortfolioPreview from './portfolio_preview/PortfolioPreview';
 
 const Welcome = () => {
   return (
@@ -11,6 +12,7 @@ const Welcome = () => {
       <Header />
       <Jumbo />
       <AboutPreview />
+      <PortfolioPreview />
     </div>
   );
 };
