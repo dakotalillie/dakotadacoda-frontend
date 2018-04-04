d22f0f0ccf31c443fee13cf2f28c7fb163f47e3e
Dakota Lillie <dakota.lillie@icloud.com>
2018-04-03 22:54:31 -0400

    add json data for cards

--- src/components/welcome/portfolio_preview/PortfolioPreview.jsx
+++ src/components/welcome/portfolio_preview/PortfolioPreview.jsx
@@ -10,68 +10,43 @@ import {
   CardTitle,
   Container
 } from 'reactstrap';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faArrowRight from '@fortawesome/fontawesome-free-solid/faArrowRight';
+import projectsData from './projects_data.json';
 import './portfolio_preview.css';
 
 const PortfolioPreview = () => {
+  const keys = Object.keys(projectsData);
+  const cards = keys.map(title => {
+    const project = projectsData[title];
+    return (
+      <Card key={project.key}>
+        <CardImg
+          top
+          width="100%"
+          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardTitle>{project.title}</CardTitle>
+          <CardSubtitle>{project.subtitle}</CardSubtitle>
+          <CardText>
+            Built With:
+            <ul>{project.technologies.map(t => <li>{t}</li>)}</ul>
+          </CardText>
+          <Button color="link" href={project.link}>
+            Github <FontAwesomeIcon icon={faArrowRight} />
+          </Button>
+        </CardBody>
+      </Card>
+    );
+  });
+
   return (
     <div className="portfolio_preview section grey-bg">
       <Container>
         <h2>Recent Projects</h2>
-        <CardDeck>
-          <Card>
-            <CardImg
-              top
-              width="100%"
-              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
-              alt="Card image cap"
-            />
-            <CardBody>
-              <CardTitle>BeetBox</CardTitle>
-              <CardSubtitle>Sample Library Manager</CardSubtitle>
-              <CardText>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </CardText>
-              <Button color="link">Github</Button>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg
-              top
-              width="100%"
-              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
-              alt="Card image cap"
-            />
-            <CardBody>
-              <CardTitle>FlatChat</CardTitle>
-              <CardSubtitle>Messaging Service</CardSubtitle>
-              <CardText>
-                This card has supporting text below as a natural lead-in to
-                additional content.
-              </CardText>
-              <Button color="link">Github</Button>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg
-              top
-              width="100%"
-              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
-              alt="Card image cap"
-            />
-            <CardBody>
-              <CardTitle>Diplomacy</CardTitle>
-              <CardSubtitle>Online Board Game</CardSubtitle>
-              <CardText>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </CardText>
-              <Button color="link">Github</Button>
-            </CardBody>
-          </Card>
-        </CardDeck>
+        <CardDeck>{cards}</CardDeck>
         <Button outline color="info" size="lg" className="section_button">
           View Portfolio
         </Button>
