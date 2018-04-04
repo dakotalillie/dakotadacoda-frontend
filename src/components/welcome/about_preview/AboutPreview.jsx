de059591cc95fcbdfb754c5b1532d25c431467c5
Dakota Lillie <dakota.lillie@icloud.com>
2018-04-03 21:02:02 -0400

    add profile image

--- src/components/welcome/about_preview/AboutPreview.jsx
+++ src/components/welcome/about_preview/AboutPreview.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Button, Container } from 'reactstrap';
+import { Button, Col, Container, Row } from 'reactstrap';
+import './about_preview.css';
+import profileImage from '../../../img/profile.jpg';
 
 const AboutPreview = () => {
   return (
     <div className="about_preview section">
       <Container>
-        <h2>My name is Dakota!</h2>
-        <h2>I write code and make music.</h2>
-        <h5>(The two are not so dissimilar).</h5>
-        <Button outline color="info" size="lg" className="section_button">
-          About Me
-        </Button>
+        <Row>
+          <Col>
+            <h2>My name is Dakota!</h2>
+            <h2>I write code and make music.</h2>
+            <h5>(The two are not so dissimilar).</h5>
+            <Button outline color="info" size="lg" className="section_button">
+              About Me
+            </Button>
+          </Col>
+          <Col>
+            <img
+              src={profileImage}
+              className="profile_image"
+              alt="profile image"
+            />
+          </Col>
+        </Row>
       </Container>
     </div>
   );
