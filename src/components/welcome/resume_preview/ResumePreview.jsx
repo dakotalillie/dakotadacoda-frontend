da2204cfa5200f47db878d662c07a7a274e810d2
Dakota Lillie <dakota.lillie@icloud.com>
2018-04-05 02:54:03 -0400

    fix issue with resume preview

--- src/components/welcome/resume_preview/ResumePreview.jsx
+++ src/components/welcome/resume_preview/ResumePreview.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Button, ButtonGroup, Col, Container, Row } from 'reactstrap';
 import { tabDisplay } from './resume_preview_utils';
 import './resume_preview.css';
@@ -13,52 +13,66 @@ class ResumePreview extends React.Component {
   };
 
   render() {
+    const section1 = (
+      <Fragment>
+        <Col sm="2" className="buttons_col">
+          <ButtonGroup vertical>
+            <Button
+              outline
+              block
+              color="primary"
+              active={this.state.active === 'languages'}
+              onClick={() => this.setActive('languages')}
+            >
+              Languages
+            </Button>
+            <Button
+              outline
+              block
+              color="primary"
+              active={this.state.active === 'frameworks'}
+              onClick={() => this.setActive('frameworks')}
+            >
+              Frameworks
+            </Button>
+            <Button
+              outline
+              block
+              color="primary"
+              active={this.state.active === 'other'}
+              onClick={() => this.setActive('other')}
+            >
+              Other
+            </Button>
+          </ButtonGroup>
+        </Col>
+        <Col sm="5" className="icons_col">
+          <Row>{tabDisplay(this.state.active)}</Row>
+        </Col>
+      </Fragment>
+    );
+
+    const section2 = (
+      <Col sm="5" className="copy_col">
+        <h2>FullStack Web Development</h2>
+        <h4>With more soon to come!</h4>
+      </Col>
+    );
+
     return (
       <div className="resume_preview section">
         <Container>
-          <Row>
-            <Col sm="2">
-              <ButtonGroup vertical>
-                <Button
-                  outline
-                  block
-                  color="primary"
-                  active={this.state.active === 'languages'}
-                  onClick={() => this.setActive('languages')}
-                >
-                  Languages
-                </Button>
-                <Button
-                  outline
-                  block
-                  color="primary"
-                  active={this.state.active === 'frameworks'}
-                  onClick={() => this.setActive('frameworks')}
-                >
-                  Frameworks
-                </Button>
-                <Button
-                  outline
-                  block
-                  color="primary"
-                  active={this.state.active === 'other'}
-                  onClick={() => this.setActive('other')}
-                >
-                  Other
-                </Button>
-              </ButtonGroup>
-            </Col>
-            <Col sm="5">
-              <Row>{tabDisplay(this.state.active)}</Row>
-            </Col>
-            <Col sm="5">
-              <h2>FullStack Web Development</h2>
-              <h4>With more soon to come!</h4>
-              {/* <Button outline color="info" className="section_button">
-                More On LinkedIn
-              </Button> */}
-            </Col>
-          </Row>
+          {window.innerWidth > 768 ? (
+            <Row>
+              {section1}
+              {section2}
+            </Row>
+          ) : (
+            <Row>
+              {section2}
+              {section1}
+            </Row>
+          )}
         </Container>
       </div>
     );
