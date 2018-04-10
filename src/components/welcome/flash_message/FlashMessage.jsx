c9a5ead38213e150be4c5b634cf90ca61e24a297
Dakota Lillie <dakota.lillie@icloud.com>
2018-04-10 00:59:11 -0400

    change flash message color

--- src/components/welcome/flash_message/FlashMessage.jsx
+++ src/components/welcome/flash_message/FlashMessage.jsx
@@ -6,7 +6,7 @@ import './flash_message.css';
 const FlashMessage = props => {
   return (
     <div className="flash_message">
-      <UncontrolledAlert color="secondary">
+      <UncontrolledAlert color="dark">
         Check out my latest blog post &mdash;{' '}
         <a href={props.link} className="alert-link">
           {props.title}
